0daf8465025294ab6d0ab0df9cdd9a4dc65dabe5
johnsey <johnsey.reynolds@gmail.com>
2022-05-08 07:44:07 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
   
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+const inputform = document.querySelector('form')
+
+inputForm.addEventListener('submit', (event) => {
+
+});
+// const init = () => {
+//     const inputForm = document.querySelector('form');
+  
+//     inputForm.addEventListener('submit', (event) => {
+//       event.preventDefault();
+  
+//     });
+//   }
+  
+  document.addEventListener('DOMContentLoaded', init);
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event);
+  });
